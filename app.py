1eca71b416b32b6ffeae6d688990c1f8cb8c6d5e
xelo1612 <169055652+xelo1612@users.noreply.github.com>
2024-06-26 12:47:45 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+
+import json 
+menup =['Ver Listado de Pinturas',
+        'Buscar Pintura',
+        'Agregar Pintura',
+        'Eliminar Pintura',
+        'Exportar Pinturas']
+
+
+lista = ['PINTURA:','CODIGO:' ]
+while True:
+    print('bienvenido a la gestion de inventario de Pinturas Acrílicas Mandarina.\n')
+    print('¿que desea hacer?')
+    for i,opt in enumerate(menup):
+        print(f'{i+1}. {opt}')
+    input('\n')
+    while True:
+        ans = (menup)
+        if ans =='1':
+            pass
+        elif ans =='2':
+            pass
+        elif ans == '3':
+            pass
+        elif ans == '4':
+            pass
+        elif ans == '5':
+            pass
+        else:
+            print('Opcion no valida!')
+        break
---
+++ modulos/f_data.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+import json
+import csv
+home = Path(__file__).parent.parent
